79d41e2fcb2cd58f4de6f2a3b6ab7a5ccccf6c8f
Jay <pjbergmann@gmail.com>
2026-02-08 18:14:33 +1100

    Update page.tsx

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function FAQ() {
   const [darkMode, setDarkMode] = useState(false);
